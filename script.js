06066480464e444f2b01cdfd6990d99433bd0014
Ibrahim Wafi Asalya <iwa1999995@gmail.com>
2025-02-14 09:55:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-// تشغيل القائمة الجانبية
-function toggleSidebar() {
-    document.querySelector('.sidebar').classList.toggle('active');
-}
 
-// تشغيل تأثير تحميل الصفحة
-window.onload = function () {
-    setTimeout(() => {
-        document.getElementById('preloader').style.display = 'none';
-    }, 1000);
-};
-
-// تحريك المهارات عند التمرير
-window.addEventListener('scroll', function () {
-    document.querySelectorAll('.progress-bar div').forEach((bar) => {
-        bar.style.width = bar.style.width;
-    });
-});
