83210c90bceb3f395f47eefdb690a59c5a62cd3a
Ibrahim Wafi Asalya <iwa1999995@gmail.com>
2025-02-18 20:45:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-
 function changeLanguage(lang) {
     // تغيير لغة الصفحة بتغيير السمة "lang" في عنصر <html>
     document.documentElement.lang = lang;
+
+    // تغيير نصوص جميع العناصر حسب اللغة
+    const elements = document.querySelectorAll('[data-lang-ar], [data-lang-en]');
     
-    // تغيير نص الزر بناءً على اللغة
+    elements.forEach(function(element) {
+        // تغيير النص بناءً على اللغة
+        if (lang === 'ar') {
+            element.textContent = element.getAttribute('data-lang-ar');
+        } else {
+            element.textContent = element.getAttribute('data-lang-en');
+        }
+    });
+
+    // تغيير نص الزر
     const langBtn = document.getElementById('lang-btn');
     if (lang === 'ar') {
         langBtn.textContent = 'تغيير اللغة';
     } else {
         langBtn.textContent = 'Change Language';
     }
-    
-    // يمكنك إضافة المزيد من التغييرات بناءً على اللغة، مثل تعديل النصوص الأخرى أو تحميل ملفات أخرى للغة
 }
